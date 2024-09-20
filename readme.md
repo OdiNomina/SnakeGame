a9bc690113890d9c8e8085780ad1590b2bd7b6eb
Silke Dittmar <silkedittmar.ae@outlook.de>
2024-09-20 13:38:43 +0200

    Add readme.md file

---
+++ readme.md
@@ -0,0 +1,5 @@
+Einfaches Konsolenspiel:
+
+Auf dem Bildschirm gibt es den Spieler, Schlangen, Gold und eine Tür.
+Die Tür und das Gold sind fest, der Spieler kann bewegt werden und die Schlangen bewegen sich selbstständig auf den Spieler zu.
+Der Spieler muss zum Gold und dann zur Tür. Wenn eine Schlange den Spieler vorher erwischt, ist das Spiel verloren.
--- src/main/java/snakeGame/ZZZnake.java
+++ src/main/java/snakeGame/ZZZnake.java
@@ -8,10 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ZZZnake {
-//	Auf dem Bildschirm gibt es den Spieler, Schlangen, Gold und eine Tür.
-//	Die Tür und das Gold sind fest, der Spieler kann bewegt werden und die Schlangen bewegen sich selbstständig auf den Spieler zu.
-//	Der Spieler muss zum Gold und dann zur Tür. Wenn eine Schlange den Spieler vorher erwischt, ist das Spiel verloren.
-	
 	public static void main(String[] args) throws IOException {
 		Console console = System.console();
 		PrintWriter consoleWriter = null;
